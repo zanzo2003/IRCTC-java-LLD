bc0faa89199802642ddc6531c8ef3b3cadfb1430
Shashwath Bhaskar <110276216+zanzo2003@users.noreply.github.com>
2025-03-16 11:49:53 +0530

    user login and register service

--- src/main/java/net/shashwathbhaskar/services/userService.java
+++ src/main/java/net/shashwathbhaskar/services/userService.java
@@ -1,23 +1,63 @@
 package net.shashwathbhaskar.services;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import net.shashwathbhaskar.models.User;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
 import java.io.File;
+import java.util.Optional;
 
 public class userService {
 
-    private User currentUser;
+    private User user;
 
-    private static final String USERS_PATH = "../localDB/users.json";
+    private List<User> usersList;
 
-    private static final String TRAINS_PATH = "../localDB/trains.json";
+    private static final String USERS_PATH = "src/main/java/net/shashwathbhaskar/localDB/users.json";
 
-    public userService(User currentUser){
-        this.currentUser = currentUser;
+    private static final String TRAINS_PATH = "src/main/java/net/shashwathbhaskar/localDB/trains.json";
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    private void writeToFile() throws IOException{
+        FileWriter users = new FileWriter(USERS_PATH);
+        users.write(objectMapper.writeValueAsString(this.usersList));
+        return ;
+    }
+
+    public userService(User currentUser) throws IOException{
+
+        this.user = currentUser;
         File users = new File(USERS_PATH);
-
+        this.usersList = objectMapper.readValue(users, new TypeReference<List<User>>() {});
 
     }
 
+    public Boolean login(User user){
+        User foundUser = null;
+        for(User currentUser: this.usersList){
+            if(currentUser.getUsername().equals(user.getUsername())
+                    && currentUser.getPassword().equals(user.getPassword())){
+                foundUser = currentUser;
+                break;
+            }
+        }
+        if(foundUser != null) this.user = foundUser;
+        return !(foundUser.equals(null));
+    }
+
+    public Boolean register(User user) throws Exception{
+        try{
+            usersList.add(user);
+            writeToFile();
+            return Boolean.TRUE;
+        }catch(Exception e){
+            throw new Exception();
+        }
+    }
 
 }
